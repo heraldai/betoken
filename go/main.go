c82a1064bc033bab94c76e215c2701b7273fd6e9
heraldai <106055336+heraldai@users.noreply.github.com>
2023-05-22 23:21:23 +0530

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,1 +1,11 @@
+// use the main package
+package main
+//import the fmt package
+import (
+"fmt"
+)
+func main(){
+fmt.Println("I am building my first blockchain") // print this
+CreateBlock("The hearder will be shown here", "the body will be shown here") // call the function that will create the block and pass some parameters in it.
 
+}
