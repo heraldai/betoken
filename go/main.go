87aabebcfc9d38e1beda45f41102d704c3e1c1f8
heraldai <106055336+heraldai@users.noreply.github.com>
2023-05-22 23:28:07 +0530

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,11 +1,17 @@
-// use the main package
+//Time to put everything together and test
 package main
-//import the fmt package
 import (
-"fmt"
+	"fmt" // just for printing something on the screen
 )
-func main(){
-fmt.Println("I am building my first blockchain") // print this
-CreateBlock("The hearder will be shown here", "the body will be shown here") // call the function that will create the block and pass some parameters in it.
-
+func main() {
+	newblockchain := NewBlockchain() // Initialize the blockchain
+	// create 2 blocks and add 2 transactions
+	newblockchain.AddBlock("first transaction")  // first block containing one tx
+	newblockchain.AddBlock("Second transaction") // second block containing one tx
+	// Now print all the blocks and their contents
+	for _, block := range newblockchain.Blocks { // iterate on each block
+		fmt.Printf("Hash of the block %x\n", block.MyBlockHash)                 // print the hash of the block
+		fmt.Printf("Hash of the previous Block: %x\n", block.PreviousBlockHash) // print the hash of the previous block
+		fmt.Printf("All the transactions: %s\n", block.AllData)                 // print the transactions
+	} // our blockchain will be printed
 }
