e2db3a5e5a01fc22c864b143b577ee1660f34840
heraldai <106055336+heraldai@users.noreply.github.com>
2023-05-22 23:22:04 +0530

    Update block.go

--- go/block.go
+++ go/block.go
@@ -1,1 +1,7 @@
-
+package main
+import (
+"fmt" // this will help us to print on the screen
+)
+func CreateBlock(Header, Body string){
+fmt.Println(Header ,"\n", Body) // Show me the block content
+}
